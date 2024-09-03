2875963ed403a2d1c8c745554cd03a3ea037c0b3
LengLKR <Lengkrupanit555@gmail.com>
2024-09-03 14:51:03 +0700

    กำจัดคำหยาบบน input ไม่ให้ใส่ลงในข้อความ

--- src/pages/addAndHistory.js
+++ src/pages/addAndHistory.js
@@ -6,33 +6,36 @@ const AddAndHistory = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [notification, setNotification] = useState(""); // State for notifications
-  const [duplicateCount, setDuplicateCount] = useState(1); // State for duplicate count
   const [showModal, setShowModal] = useState(false);
   const [showRecommend, setRecommend] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    // ดึงข้อความจาก API
     const fetchMessages = async () => {
       try {
         const response = await axios.get("http://localhost:8888/api/messages");
-        const filteredMessages = response.data.map((msg) => {
-          return {
-            ...msg,
-            text: badWords.reduce(
-              (acc, word) => acc.replace(new RegExp(word, "gi"), "***"),
-              msg.text
-            ),
-          };
-        });
-        setMessage(filteredMessages);
+        if (response.data.length > 0) {
+          const filteredMessages = response.data.map((msg) => {
+            return {
+              ...msg,
+              text: badWords.reduce(
+                (acc, word) => acc.replace(new RegExp(word, "gi"), "***"),
+                msg.text
+              ),
+            };
+          });
+          setMessages(filteredMessages);
+        } else {
+          setNotification("No messages found.");
+        }
       } catch (error) {
         console.error("Error fetching messages:", error);
-        setNotification("Failed to fetch messages.");
+        setNotification("");
       }
     };
     fetchMessages();
   }, []);
+  
 
   const handleSave = async () => {
     const badWords = [
@@ -56,8 +59,88 @@ const AddAndHistory = () => {
       "ไอ้หน้าโง่",
       "กระโหลก",
       "อีสัส",
+      "ขว$ย",
+      "vตวu[[y6i",
+      "xีสั$",
+      "ค*ย",
+      "hี",
+      "ืเu",
+      "ควาi",
+      "l]งซวย",
+      "อีต@แหล",
+      "ไอระยำ",
+      "aัส",
+      "xัส",
+      "vtwic",
+      "ก$ย",
+      "คsย",
+      "ฆวย",
+      "คๅย",
+      "คุวย",
+      "อีแก่",
+      "อีบ้า",
+      "อีโง่",
+      "ไอ้ขี้เมา",
+      "ไอ้หน้าแหก",
+      "ไอ้ตอแหล",
+      "อีชั่ว",
+      "อีอ้วน",
+      "อีบัดซบ",
+      "ไอ้ถ่อย",
+      "อีสันดาน",
+      "ไอ้หัวขวด",
+      "อีปอบ",
+      "ไอ้จังไร",
+      "อีชิงหมาเกิด",
+      "ไอ้ขี้โกง",
+      "ไอ้ขี้ขโมย",
+      "ขวาย",
+      "สันขวาน",
+      "xี",
+      "มึง",
+      "กู",
+      "ไอกาก",
+      "fuck",
+      "shit",
+      "bitch",
+      "asshole",
+      "bastard",
+      "damn",
+      "cunt",
+      "dick",
+      "piss",
+      "whore",
+      "slut",
+      "fag",
+      "nigger",
+      "motherfucker",
+      "cock",
+      "pussy",
+      "wanker",
+      "jerk",
+      "douchebag",
+      "twat",
+      "prick",
+      "bollocks",
+      "bugger",
+      "arse",
+      "tosser",
+      "skank",
+      "scumbag",
+      "dickhead",
+      "shithead",
+      "fucker",
+      "cocksucker",
+      "twathead",
+      "asswipe",
+      "crap",
+      "hell",
+      "bloody",
+      "blowjob",
+      "sod",
+      "son of a bitch",
+      "ๆอสัส",
     ];
-
     const containsBadWords = (text) => {
       return badWords.some((word) => text.includes(word));
     };
@@ -78,7 +161,7 @@ const AddAndHistory = () => {
         "http://localhost:8888/api/saveMessage",
         { message }
       );
-  
+
       setMessages([response.data, ...messages]);
       setMessage(""); // Clear the textarea after saving
       setNotification("Message saved successfully.");
@@ -102,7 +185,6 @@ const AddAndHistory = () => {
   const toggleRecommend = () => {
     setRecommend(!showRecommend);
   };
-
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
@@ -125,84 +207,110 @@ const AddAndHistory = () => {
           </div>
         </div>
       </button>
-      <div className="w-full max-w-lg p-8 ">
-        <h1 className="text-2xl font-bold text-center text-white">
-          แบ่งปันข้อความดี ๆ ให้กับคนที่คุณรักและห่วงใย
-        </h1>
-        <p className="text-center text-white mt-2">
-          คุณสามารถเลือกใช้{" "}
-          <button onClick={toggleRecommend}>
-            <u>ข้อความแนะนำ</u>
-          </button>{" "}
-          ของ <strong>Daily Boost</strong> เพื่อส่งไปให้คนที่คุณรักได้
-        </p>
-        {notification && (
-          <div className="bg-violet-400 p-2 mt-4 rounded text-black">
-            {notification}
-            <button
-              className="ml-4 text-red-600"
-              onClick={handleNotificationClose}
-            >
-              X
-            </button>
-          </div>
-        )}
-        <textarea
-          className="w-full p-4 mt-6 text-black bg-gray-200 rounded-lg resize-none"
-          rows={4}
-          placeholder="แบ่งปันข้อความของคุณ..."
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        ></textarea>
-        <button
-          className="mt-4 w-full px-6 py-2 text-white bg-purple-600 rounded-full shadow hover:bg-purple-700 transition-colors"
-          onClick={handleSave}
-        >
-          ส่งข้อความ
-        </button>
-
-        <h1 className=" text-sm font-bold mt-8 text-center text-white">
-          ประวัติข้อความ
-        </h1>
-        <ul className="mt-4 max-h-64 overflow-y-auto">
-          {messages.map((msg) => (
-            <li
-              key={msg.id}
-              className="border p-2 mb-2 bg-white bg-opacity-70 rounded-lg shadow"
-            >
-              <p>{msg.text}</p>{" "}
-            </li>
-          ))}
-        </ul>
-        {/* ปุ่มที่มุมขวาล่าง */}
-        <div className="fixed bottom-4 right-4 z-50">
-          <button
-            onClick={toggleMedal}
-            className="text-white text-sm underline hover:text-gray-300 transition-colors"
-          >
-            คำแนะนำการใช้งาน
-          </button>
-        </div>
-        {/* ป๊อปอัพสำหรับแสดงคำแนะนำการใช้งาน */}
-        {showModal && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg max-w-md mx-auto">
-              <h2 className="text-lg font-bold mb-4">คำแนะนำการใช้งาน</h2>
-              <p className="text-gray-700">
-                นี่คือคำแนะนำในการใช้งานของแอปพลิเคชันนี้...
-              </p>
+      <div className="w-full max-w-4xl p-8 flex">
+        {/* ฟอร์มแบ่งปันข้อความ */}
+        <div className="flex-1">
+          <h1 className="text-2xl font-bold text-center text-white">
+            แบ่งปันข้อความดี ๆ ให้กับคนที่คุณรักและห่วงใย
+          </h1>
+          <p className="text-center text-white mt-2">
+            คุณสามารถเลือกใช้{" "}
+            <button onClick={toggleRecommend}>
+              <u>ข้อความแนะนำ</u>
+            </button>{" "}
+            ของ <strong>Daily Boost</strong> เพื่อส่งไปให้คนที่คุณรักได้
+          </p>
+  
+          {showRecommend && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+              <div className="bg-white p-6 rounded-lg shadow-lg max-w-md mx-auto">
+                <h2 className="text-lg font-bold mb-4">ข้อความแนะนำ</h2>
+                <p className="text-gray-700 text-black">
+                  นี่คือตัวอย่างข้อความที่คุณสามารถใช้ส่งไปให้คนที่คุณรักได้...
+                </p>
+                <button
+                  onClick={toggleRecommend}
+                  className="mt-4 px-4 py-2 text-white font-bold bg-purple-500 rounded-full shadow-lg hover:bg-purple-600 transition-colors w-full"
+                >
+                  ปิด
+                </button>
+              </div>
+            </div>
+          )}
+          {notification && (
+            <div className="bg-violet-400 p-2 mt-4 rounded text-black">
+              {notification}
               <button
-                onClick={toggleMedal}
-                className="mt-4 px-4 py-2 text-white font-bold bg-purple-500 rounded-full shadow-lg hover:bg-purple-600 transition-colors w-full"
+                className="ml-4 text-red-600"
+                onClick={handleNotificationClose}
               >
-                ปิด
+                X
               </button>
             </div>
-          </div>
-        )}
+          )}
+          <textarea
+            className="w-full p-4 mt-6 text-black bg-gray-200 rounded-lg resize-none"
+            rows={4}
+            placeholder="แบ่งปันข้อความของคุณ..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+          <button
+            className="mt-4 w-full px-6 py-2 text-white bg-purple-600 rounded-full shadow hover:bg-purple-700 transition-colors"
+            onClick={handleSave}
+          >
+            ส่งข้อความ
+          </button>
+        </div>
+  
+        {/* ประวัติข้อความ */}
+        <div className="flex-1 ml-24">
+          <h1 className="text-2xl font-bold mt-8 text-center text-white">
+            ประวัติข้อความ
+          </h1>
+          <ul className="mt-4 max-h-64 overflow-y-auto">
+            {messages.map((msg) => (
+              <li
+                key={msg.id}
+                className="border p-2 mb-2 bg-white bg-opacity-70 rounded-lg shadow"
+              >
+                <p>{msg.text}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+  
+      {/* ปุ่มที่มุมขวาล่าง */}
+      <div className="fixed bottom-4 right-4 z-50">
+        <button
+          onClick={toggleMedal}
+          className="text-white text-sm underline hover:text-gray-300 transition-colors"
+        >
+          คำแนะนำการใช้งาน
+        </button>
+      </div>
+  
+      {/* ป๊อปอัพสำหรับแสดงคำแนะนำการใช้งาน */}
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg max-w-md mx-auto">
+            <h2 className="text-lg font-bold mb-4">คำแนะนำการใช้งาน</h2>
+            <p className="text-gray-700">
+              นี่คือคำแนะนำในการใช้งานของแอปพลิเคชันนี้...
+            </p>
+            <button
+              onClick={toggleMedal}
+              className="mt-4 px-4 py-2 text-white font-bold bg-purple-500 rounded-full shadow-lg hover:bg-purple-600 transition-colors w-full"
+            >
+              ปิด
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
+  
 };
 
 export default AddAndHistory;
